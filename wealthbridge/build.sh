e9c98505537793f2aa99d3d87703630445f7f5b2
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-21 15:45:59 +0100

    Update build.sh
    Save

--- wealthbridge/build.sh
+++ wealthbridge/build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Collect static files
 python manage.py collectstatic --no-input
 
+# Make migrations
+python manage.py makemigrations
+
 # Apply database migrations
 python manage.py migrate
 
