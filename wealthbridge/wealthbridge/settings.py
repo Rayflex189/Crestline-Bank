7fdf144c44552cc25fc7e922cb9f3d3edacef466
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-21 07:51:49 +0100

    Update settings.py
    Save

--- wealthbridge/wealthbridge/settings.py
+++ wealthbridge/wealthbridge/settings.py
@@ -51,7 +51,7 @@ SECRET_KEY = 'django-insecure-3_)^u&niz%-isn%ciqt+qx7*3h!bo(js3+s%x0qray8bkb8d_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['crestline-finance.onrender.com']
+ALLOWED_HOSTS = ['crestline-bank.fly.dev']
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dlzn0moho',
